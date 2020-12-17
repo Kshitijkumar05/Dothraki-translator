67bcb8dc3d55193f1fdc70fb6bc5c8594fb35dd2
Kshitijkumar05 <Kshitijkumar05@users.noreply.github.com>
2020-12-17 21:46:49 +0530

    URL update

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var btnTranslate=document.querySelector("#btn-translate");
 var txtInput=document.querySelector("#txt-input");
 var outputDiv=document.querySelector("#translate-output");
 
-var serverURL = "https://api.funtranslations.com/translate/valyrian.json"
+var serverURL = "https://api.funtranslations.com/translate/dothraki.json"
 
 function getTranslationURL(input){
     return serverURL + "?" +"text=" + input
